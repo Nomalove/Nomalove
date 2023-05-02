3a7fcbc66dfcf897b5e04b897bf513c860f36ccc
NomaLove <nomandladube2@gmail.com>
2023-05-02 18:29:59 +0200

    import function, created title name and description

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { NomaloveComponent } from './App'
+import { NomaloveComponent } from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NomaloveComponent/>
+    <NomaloveComponent title={'My App'} description={'My best app'}/>
   </React.StrictMode>,
 )
